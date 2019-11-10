64b4e46581d2e6ad620d755e84b32cf1e4743bb5
Ilya Laryushkin <ilyalaryushkin@gmail.com>
2019-11-10 23:24:15 +0300

    Продолжение решения коричневого пояса Решены задачи: - hash_point - hash_person
    Вторичный индекс реализованный через unordered_map. Time limit

--- 4. Brown/1 Week/6 task/secondary_index.cpp
+++ 4. Brown/1 Week/6 task/secondary_index.cpp
@@ -1,5 +1,5 @@
-//#include "test_runner.h"
-#include "../../../MyUtils/MyTestFramework/TestFramework.h"
+#include "test_runner.h"
+//#include "../../../MyUtils/MyTestFramework/TestFramework.h"
 #include <iostream>
 #include <map>
 #include <string>
@@ -23,8 +23,8 @@ public:
   bool Put(const Record& record) {
     if (!database_.count(record.id)) {
       database_[record.id] = record;
-      timestamp_range[record.timestamp] = record.id;
-      karma_range[record.karma] = record.id;
+      timestamp_range[record.timestamp].insert(record.id);
+      karma_range[record.karma].insert(record.id);
       users_ids[record.user].insert(record.id);
       return true;
     }
@@ -44,8 +44,8 @@ public:
 
   bool Erase(const string& id) {
     if (database_.count(id)) {
-      timestamp_range.erase(database_[id].timestamp);
-      karma_range.erase(database_[id].karma);
+      timestamp_range[database_[id].timestamp].erase(id);
+      karma_range[database_[id].karma].erase(id);
       users_ids[database_[id].user].erase(id);
       database_.erase(id);
       return true;
@@ -60,7 +60,12 @@ public:
     auto range_begin = timestamp_range.lower_bound(low);
     auto range_end = timestamp_range.upper_bound(high);
     if (range_begin == timestamp_range.end()) return;
-    for (auto i = range_begin; i != range_end && callback(database_.at(i->second)); ++i);
+    bool flag = true;
+    for (auto i = range_begin; i != range_end && flag; ++i) {
+      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+        flag = callback(database_.at(*j));
+      }
+    }
   }
 
   template <typename Callback>
@@ -68,7 +73,12 @@ public:
     auto range_begin = karma_range.lower_bound(low);
     auto range_end = karma_range.upper_bound(high);
     if (range_begin == karma_range.end()) return;
-    for (auto i = range_begin; i != range_end && callback(database_.at(i->second)); ++i);
+    bool flag = true;
+    for (auto i = range_begin; i != range_end && flag; ++i) {
+      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+        flag = callback(database_.at(*j));
+      }
+    }
   }
 
   template <typename Callback>
@@ -83,9 +93,9 @@ public:
   // ID - Record
   std::unordered_map<std::string, Record> database_;
   // Timestamp - ID
-  std::map<int, std::string> timestamp_range;
+  std::map<int, std::unordered_set<std::string>> timestamp_range;
   // Karma - ID
-  std::map<int, std::string> karma_range;
+  std::map<int, std::unordered_set<std::string>> karma_range;
   // User - ID
   std::unordered_map<std::string, std::unordered_set<std::string>> users_ids;
 };
@@ -96,6 +106,7 @@ void TestRangeBoundaries() {
 
   Database db;
   db.Put({"id1", "Hello there", "master", 1536107260, good_karma});
+  db.Put({"id11", "Hello there", "master", 1536107260, good_karma});
   db.Put({"id2", "Hello there", "master", 1536107261, good_karma - 1});
   db.Put({"id3", "O>>-<", "general2", 1536107262, bad_karma + 1});
   db.Put({"id4", "O>>-<", "general2", 1536107260, bad_karma});
@@ -106,7 +117,7 @@ void TestRangeBoundaries() {
     return true;
   });
 
-  ASSERT_EQUAL(4, count);
+  ASSERT_EQUAL(5, count);
 }
 
 void TestSameUser() {
