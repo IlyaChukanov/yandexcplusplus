b2bb83ea441b407627227ab4ae2487dad35a76c8
Ilya Laryushkin <ilyalaryushkin@gmail.com>
2019-11-12 14:48:27 +0300

    Продолжение решения коричневого пояса Вторичный индекс с итераторами

--- 4. Brown/1 Week/6 task/secondary_index.cpp
+++ 4. Brown/1 Week/6 task/secondary_index.cpp
@@ -1,5 +1,6 @@
-#include "test_runner.h"
-//#include "../../../MyUtils/MyTestFramework/TestFramework.h"
+//#include "test_runner.h"
+#include "../../../MyUtils/MyTestFramework/TestFramework.h"
+//#include "../../../MyUtils/Profiler/profiler.h"
 #include <iostream>
 #include <map>
 #include <string>
@@ -20,12 +21,41 @@ struct Record {
 // Реализуйте этот класс
 class Database {
 public:
+
+  Database() {
+    //time_dur = TotalDuration("- Time Duration");
+    //karma_dur = TotalDuration("- Karma Duration");
+    //user_dur = TotalDuration("- User Duration");
+    //db_dur = TotalDuration("- DB Duration");
+  }
+
   bool Put(const Record& record) {
-    if (!database_.count(record.id)) {
-      database_[record.id] = record;
-      timestamp_range[record.timestamp].insert(record.id);
-      karma_range[record.karma].insert(record.id);
-      users_ids[record.user].insert(record.id);
+    auto record_id = record.id;
+    if (!database_.count(record_id)) {
+
+      auto timestamp_inserted = timestamp_range.insert(std::make_pair(record.timestamp, record.id));
+      auto karma_inserted = karma_range.insert(std::make_pair(record.karma, record.id));
+      users_ids[record.user].insert(record_id);
+      database_[record_id] = {record, timestamp_inserted, karma_inserted};
+
+      /*TimeStampIndex::iterator timestamp_inserted;
+      KarmaIndex::iterator karma_inserted;
+      {
+        ADD_DURATION(time_dur);
+        timestamp_inserted = timestamp_range.insert(std::make_pair(record.timestamp, record_id));
+      }
+      {
+        ADD_DURATION(karma_dur)
+        karma_inserted = karma_range.insert(std::make_pair(record.karma, record_id));
+      }
+      {
+        ADD_DURATION(user_dur);
+        users_ids[record.user].insert(record_id);
+      }
+      {
+        ADD_DURATION(db_dur);
+        database_[record_id] = {std::move(record), timestamp_inserted, karma_inserted};
+      }*/
       return true;
     }
     else {
@@ -35,7 +65,7 @@ public:
 
   const Record* GetById(const string& id) const {
     if (database_.count(id)) {
-      return &database_.at(id);
+      return &database_.at(id).record_;
     }
     else {
       return nullptr;
@@ -44,9 +74,9 @@ public:
 
   bool Erase(const string& id) {
     if (database_.count(id)) {
-      timestamp_range[database_[id].timestamp].erase(id);
-      karma_range[database_[id].karma].erase(id);
-      users_ids[database_[id].user].erase(id);
+      timestamp_range.erase(database_[id].timestamp_record_);
+      karma_range.erase(database_[id].karma_record_);
+      users_ids[database_[id].record_.user].erase(id);
       database_.erase(id);
       return true;
     }
@@ -60,12 +90,7 @@ public:
     auto range_begin = timestamp_range.lower_bound(low);
     auto range_end = timestamp_range.upper_bound(high);
     if (range_begin == timestamp_range.end()) return;
-    bool flag = true;
-    for (auto i = range_begin; i != range_end && flag; ++i) {
-      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
-        flag = callback(database_.at(*j));
-      }
-    }
+    for (auto i = range_begin; i != range_end && callback(database_.at(i->second).record_); ++i);
   }
 
   template <typename Callback>
@@ -74,11 +99,7 @@ public:
     auto range_end = karma_range.upper_bound(high);
     if (range_begin == karma_range.end()) return;
     bool flag = true;
-    for (auto i = range_begin; i != range_end && flag; ++i) {
-      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
-        flag = callback(database_.at(*j));
-      }
-    }
+    for (auto i = range_begin; i != range_end && callback(database_.at(i->second).record_); ++i);
   }
 
   template <typename Callback>
@@ -86,18 +107,31 @@ public:
     auto finded = users_ids.find(user);
     if (finded == users_ids.end()) return;
     auto cont = finded->second;
-    for (auto id = finded->second.begin(); id != finded->second.end() && callback(database_.at(*id)); ++id);
+    for (auto id = finded->second.begin(); id != finded->second.end() && callback(database_.at(*id).record_); ++id);
   }
 
  private:
+
+  using TimeStampIndex = std::multimap<int, std::string>;
+  using KarmaIndex = std::multimap<int, std::string>;
+  using UserIndex = std::unordered_map<std::string, std::unordered_set<std::string>>;
+
+  struct DBRecord {
+    Record record_;
+    TimeStampIndex::iterator timestamp_record_;
+    KarmaIndex::iterator karma_record_;
+    //UserIndex::iterator user_index_;
+  };
+
+  //TotalDuration time_dur, karma_dur, user_dur, db_dur;
   // ID - Record
-  std::unordered_map<std::string, Record> database_;
+  std::unordered_map<std::string, DBRecord> database_;
   // Timestamp - ID
-  std::map<int, std::unordered_set<std::string>> timestamp_range;
+  TimeStampIndex timestamp_range;
   // Karma - ID
-  std::map<int, std::unordered_set<std::string>> karma_range;
+  KarmaIndex karma_range;
   // User - ID
-  std::unordered_map<std::string, std::unordered_set<std::string>> users_ids;
+  UserIndex users_ids;
 };
 
 void TestRangeBoundaries() {
@@ -147,10 +181,83 @@ void TestReplacement() {
   ASSERT_EQUAL(final_body, record->title);
 }
 
+
+/*void HighLoad() {
+  Database db;
+
+  const size_t PUT_COUNT = 300000;
+  {
+    LOG_DURATION("PUT TIME")
+    int id = 0;
+    for (size_t i = 0; i < PUT_COUNT; ++i) {
+      db.Put({"id" + std::to_string(id), "Text", "Name" + std::to_string(id / 10), 1000 + id, 10 + id % 10});
+      id++;
+    }
+  }
+  const size_t ERASE_COUNT = 100000;
+  {
+    LOG_DURATION("ERASE TIME")
+    int id = 0;
+    for (size_t i = 0; i < ERASE_COUNT; ++i) {
+      db.Erase("id" + std::to_string(id));
+      id += 2;
+    }
+  }
+  const size_t GET_COUNT = 100000;
+  {
+    LOG_DURATION("GET TIME")
+    int id = 0;
+    for (size_t i = 0; i < ERASE_COUNT; ++i) {
+      db.GetById("id" + std::to_string(id));
+      id += 3;
+    }
+  }
+
+  const size_t TIMESTAMP_COUNT = 100000;
+  {
+    LOG_DURATION("TIMESTAMP TIME")
+    for (size_t i = 0; i < ERASE_COUNT; ++i) {
+      int count = 0;
+      db.RangeByTimestamp(i + 1000, i + 1100, [&count](const Record&) {
+        ++count;
+        return true;
+      });
+    }
+  }
+
+  const size_t KARMA_COUNT = 100000;
+  {
+    LOG_DURATION("KARMA TIME")
+    for (size_t i = 0; i < ERASE_COUNT; ++i) {
+      int count = 0;
+      db.RangeByKarma(i + 10, i + 30, [&count](const Record&) {
+        ++count;
+        return true;
+      });
+    }
+  }
+
+  const size_t NAME_COUNT = 100000;
+  {
+    LOG_DURATION("NAME TIME")
+    std::string name = "Name";
+    for (size_t i = 0; i < ERASE_COUNT; ++i) {
+      int count = 0;
+      db.AllByUser(name + std::to_string(i / 10), [&count](const Record&) {
+        ++count;
+        return true;
+      });
+    }
+  }
+
+
+}*/
+
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestRangeBoundaries);
   RUN_TEST(tr, TestSameUser);
   RUN_TEST(tr, TestReplacement);
+  //RUN_TEST(tr, HighLoad);
   return 0;
 }
